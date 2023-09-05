f603d735260be4f5374d924e4f4bdfabd4590550
ForánDavidDurango1725 <forand.d305@gmail.com>
2023-09-04 23:35:26 -0500

    Agregar archivos de proyecto.

---
+++ tablademultiplicar/Program.cs
@@ -0,0 +1,28 @@
+﻿
+
+namespace tablademultiplicar
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }using System;
+
+class TablaMultiplicar
+    {
+        static void Main()
+        {
+            int numero, resultado;
+
+            Console.Write("Ingrese un número: ");
+            numero = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= 10; i++)
+            {
+                resultado = numero * i;
+                Console.WriteLine(numero + " x " + i + " = " + resultado);
+            }
+        }
+    }
+}
+}
